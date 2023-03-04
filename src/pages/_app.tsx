65e13d1171ab1386e12482369d8c3e165e7df390
devgl96 <georgelucas.dev@gmail.com>
2023-03-04 17:45:10 -0300

    Creating authentication using next-auth with github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,19 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
 
 import { Header } from "../components/Header";
 
-// import { SessionProvider as NextAuthProvider } from "next-auth/react";
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
 
 export default function App({
   Component,
-  pageProps,
-}: // pageProps: { session, ...pageProps }
-AppProps) {
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   return (
-    <>
-      {/* <NextAuthProvider session={session}> */}
+    <NextAuthProvider session={session}>
       <Header />
       <Component {...pageProps} />
-      {/* </NextAuthProvider> */}
-    </>
+    </NextAuthProvider>
   );
 }
