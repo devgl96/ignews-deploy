13aeacd239025f6aad8cc77448d0bb26a27cb5aa
devgl96 <georgelucas.dev@gmail.com>
2023-03-04 18:25:43 -0300

    Using the callbacks for the faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import { fauna } from "@/services/fauna";
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
@@ -14,6 +16,79 @@ export const authOptions = {
       },
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
+    async signIn({ user }) {
+      const { email } = user;
+
+      // console.log("email: ", email);
+
+      // return true;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(email) // Tudo lowercase
+                )
+              )
+            ),
+            // Verdadeiro - IF
+            q.Create(q.Collection("users"), {
+              data: { email },
+            }),
+            // Falso - ELSE
+            q.Get(
+              // select
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email) // Tudo lowercase
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch (err) {
+        console.error("error => ", err);
+        return false;
+      }
+    },
+  },
 };
 
 export default NextAuth(authOptions);
