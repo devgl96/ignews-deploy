262d9e636f8359422ee883c3fed5ba97d19f7373
devgl96 <georgelucas.dev@gmail.com>
2023-03-04 16:22:00 -0300

    Updating the _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { Header } from "../components/Header";
+
+// import { SessionProvider as NextAuthProvider } from "next-auth/react";
+
+export default function App({
+  Component,
+  pageProps,
+}: // pageProps: { session, ...pageProps }
+AppProps) {
+  return (
+    <>
+      {/* <NextAuthProvider session={session}> */}
+      <Header />
+      <Component {...pageProps} />
+      {/* </NextAuthProvider> */}
+    </>
+  );
 }
