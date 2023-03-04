ce774bdbe792ac8204935cf6aa489c2f989fca88
devgl96 <georgelucas.dev@gmail.com>
2023-03-04 19:03:09 -0300

    Updating the SubscribeButton to access the subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,44 +1,44 @@
-// import { signIn, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-// import { api } from "../../services/api";
-// import { getStripeJs } from "../../services/stripe-js";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 export function SubscribeButton() {
-  // const { data: session } = useSession();
-  // const router = useRouter();
-  // // console.log("Session: ", session);
+  const { data: session } = useSession();
+  const router = useRouter();
+  console.log("Session: ", session);
 
-  // async function handleSubscribe() {
-  //   if (!session) {
-  //     signIn("github");
-  //     return;
-  //   }
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
 
-  //   if (session.activeSubscription) {
-  //     router.push("/posts");
-  //     return;
-  //   }
+    if (session?.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
 
-  //   // Criação da Checkout Session
-  //   try {
-  //     const response = await api.post("/subscribe");
+    // Criação da Checkout Session
+    try {
+      const response = await api.post("/subscribe");
 
-  //     const { sessionId } = response.data;
+      const { sessionId } = response.data;
 
-  //     const stripe = await getStripeJs();
+      const stripe = await getStripeJs();
 
-  //     await stripe.redirectToCheckout({ sessionId });
-  //   } catch (err) {
-  //     alert(err.message);
-  //   }
-  // }
+      await stripe?.redirectToCheckout({ sessionId });
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
-      // onClick={handleSubscribe}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
