8909b12ff274dd2568b429ae1859ded1e3373b33
devgl96 <georgelucas.dev@gmail.com>
2023-03-04 23:09:30 -0300

    Updating the getPrismicClient function in the service

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,6 +1,7 @@
 import * as Prismic from "@prismicio/client";
+import { IncomingMessage } from "http";
 
-export function getPrismicClient(ref?: string) {
+export function getPrismicClient(ref?: string | IncomingMessage) {
   const endpoint = Prismic.getRepositoryEndpoint("ignewsgl");
 
   const prismic = Prismic.createClient(endpoint, {
